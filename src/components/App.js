a0b80c7143b6d6e63bb8e3f816d66ec29126fad7
YERIM <dnaer456@gmail.com>
2020-08-10 20:34:46 +0900

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import MM00_v from "./screens/MM00_v";
 import { Route } from "react-router-dom";
+import MM00_v from "./screens/MM00_v";
+import FreeBoard from "./screens/FreeBoard";
+import DocsBoard from "./screens/DocsBoard";
+import GallaryBoard from "./screens/GallaryBoard";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={MM00_v} />
+        <Route exact path="/freeboard" component={FreeBoard} />
+        <Route exact path="/docsboard" component={DocsBoard} />
+        <Route exact path="/gallaryboard" component={GallaryBoard} />
       </div>
     );
   }
