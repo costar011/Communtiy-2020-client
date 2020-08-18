9e29d3a0720efa21032a46b7cfa4a4c2e77b6444
YERIM <dnaer456@gmail.com>
2020-08-18 21:24:45 +0900

    wrtie

--- src/components/screens/Write.js
+++ src/components/screens/Write.js
@@ -1,21 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { C_Btn, D_Btn } from "../commonComponents";
 
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => props.direction || `column`};
   align-items: center;
   justify-content: center;
   padding: 30px;
 `;
 
-const Title = styled.div`
+const Title = styled.h2`
   font-size: 28px;
+  margin-bottom: 60px;
 `;
 
 const TextInput = styled.input`
-  width: 440px;
+  width: 640px;
   height: 30px;
   border-radius: 4px;
   outline: none;
@@ -30,7 +32,7 @@ const TextInput = styled.input`
 `;
 
 const TextArea = styled.textarea`
-  width: 440px;
+  width: 640px;
   height: 300px;
   border-radius: 4px;
   outline: none;
@@ -47,14 +49,20 @@ const TextArea = styled.textarea`
 
 class Write extends React.Component {
   render() {
-    console.log(this.props.match.params.boardType);
     return (
       <Wrapper>
-        <Title>게시글 작성하기</Title>
+        <Title>
+          {console.log(this.props.match.params.boardType)}게시글 작성하기
+        </Title>
 
         <TextInput type="text" placeholder="Title..." />
         <TextInput type="text" placeholder="Author..." />
         <TextArea placeholder="Description..." />
+
+        <Wrapper direction={`row`}>
+          <C_Btn>작성하기</C_Btn>
+          <D_Btn>작성취소</D_Btn>
+        </Wrapper>
       </Wrapper>
     );
   }
